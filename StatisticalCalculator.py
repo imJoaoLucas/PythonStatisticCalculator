fbe70deb9ff4edd56c084173554e941f4160f16c
Joao Lucas Classio <jl.classio@gmail.com>
2025-10-29 20:16:40 -0400

    Create StatisticalCalculator.py

---
+++ StatisticalCalculator.py
@@ -0,0 +1,49 @@
+#Joao Classio   #10/29/2025     #statisticalCalculator
+'''
+I build this program during my statistical classes in order to practice both python
+and statistics. It might receive some updates in the future.
+'''
+import math
+
+userEntryList = [] 
+
+def userEntry():
+    entry = "y"
+    while entry != "":
+        try:
+            entry = input("Enter a numeric value for the list or ENTER to finish the list")
+            if entry != "":
+                userEntryList.append(float(entry))
+        except():
+            print("Invalid value")
+    return userEntryList
+
+def displayMenu():
+    choice = 0
+    measuresSelected = []
+    while choice <1 or choice >5:
+        print('''\n    Select the measures you want to display
+        ----------------------------------------------------
+        1. Mean
+        2. Median
+        3. Mode
+        4. Variance
+        5. Standard Deviation
+        6. Continue...
+        ''')
+        if measuresSelected:
+            print("Your selection --> " + " ".join(str(num) for num in measuresSelected))
+        try:
+            choice=int(input('Enter 1,2,3,4,5 or 6  '))
+            if choice == 6:
+                break
+            if choice >= 1 and choice <= 5 and not choice in measuresSelected:
+                measuresSelected.append(choice)
+                
+        except:
+            input('\n\nINVALID SELECTION - PRESS ENTER & TRY AGAIN ')
+            choice=0
+        
+        choice = 0
+
+displayMenu()
